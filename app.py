7aaae7489d2085eb94a882802d005fdd426f8148
rohit <rowhittank@gmail.com>
2024-10-06 12:48:44 +0530

    cleaned analytics endpoint and credentials added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, status, HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import httpx
 from models import QnAnswers
-
+from helper import get_score
 
 app = FastAPI(title='Analytics')
 security = HTTPBearer()
@@ -16,53 +16,25 @@ async def home():
 
 
 
-@app.get('/analytics/{qna_id}', tags=['analytics'])
-async def analysis_on_qna(qna_id):
+@app.get('/analytics/{qna_id}', tags=['analytics'], status_code=status.HTTP_200_OK)
+async def analysis_on_qna(qna_id, credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)]):
+
     async with httpx.AsyncClient() as client:
-        r = await client.get(f'http://localhost:8081/test_get_qna/{qna_id}')
+        r = await client.get(f'http://localhost:8083/get_qna/{qna_id}', headers={'Authorization': f'{credentials.scheme} {credentials.credentials}'})
         if r.status_code != status.HTTP_200_OK:
             raise HTTPException(status_code=r.status_code)
 
-    
-    # @TODO
-    # Handle None -  if no question, or answer
-    
     res = r.json()
-    qna = res.get('ques')
-    answers = res.get('ans')
+    ques = res.get('ques')
+    ans = res.get('ans')    
 
-    print(answers)
-
-    total_participants = len(answers)
-    count = 0
-
-    print(answers[0][0].get('choice'))
-    for j in range(total_participants):        
-        sum = 0
-        for i in range(len(qna)):
-            if qna[i].get('mcq'):
-                if qna[i].get('correct') == answers[j][i].get('choice'):
-                    print(qna[i].get('correct'), " ", answers[j][i].get('choice'))
-                    sum+=1
-                else: 
-                    sum-=1
-            else:
-                # @TODO
-                # if the answer is text, check using some AI model?
-                # for now assuming it correct
-                sum+=1
-        if sum == len(qna):
-            count+=1
-
-    score = f'{count}/{len(answers)}'
-    print(f'\n\n{score}')
-
-    return score
+    score = get_score(ques=ques, ans=ans)
+    
+    return {score: f"{score}"} 
     
 
     
 
-
-@app.get('/anal')
-async def test(qna: QnAnswers):
+@app.get('/help')
+async def get_help(help: QnAnswers):
     pass
--- helper.py
+++ helper.py
@@ -1,3 +1,28 @@
 import httpx
+from typing import List, Dict
 
 
+def get_score(ques: List[Dict], ans: List[List[Dict]]):
+    total_participants = len(ans)
+    count = 0
+
+    # print(ans[0][0].get('choice'))
+    for j in range(total_participants):        
+        sum = 0
+        for i in range(len(ques)):
+            if ques[i].get('mcq'):
+                if ques[i].get('correct') == ans[j][i].get('choice'):
+                    print(ques[i].get('correct'), " ", ans[j][i].get('choice'))
+                    sum+=1
+                else: 
+                    sum-=1
+            else:
+                # @TODO
+                # if the answer is text, check using some AI model?
+                # for now assuming it correct
+                sum+=1
+        if sum == len(ques):
+            count+=1
+
+    score = f'{count}/{len(ans)}'
+    return score
